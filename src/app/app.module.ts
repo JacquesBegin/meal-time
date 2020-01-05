ab0a244115d3317f8fdf710355a04762ce4e9124
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-05 11:42:53 -0500

    Creates list-edit component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
 import { ListComponent } from './shoppingList/list/list.component';
+import { ListEditComponent } from './shoppingList/list-edit/list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListComponent
+    ListComponent,
+    ListEditComponent
   ],
   imports: [
     BrowserModule,
