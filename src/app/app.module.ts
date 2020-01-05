e4d60189d1cb0c12402f9ff9d5be0a7d9bfaa000
Jacques Begin <jacques.andre.begin@gmail.com>
2020-01-05 11:50:17 -0500

    Creates shopping list component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
-import { ListComponent } from './shoppingList/list/list.component';
-import { ListEditComponent } from './shoppingList/list-edit/list-edit.component';
 import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shoppingList/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ListComponent,
-    ListEditComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
